25d25df69871c3aa7b3edd1d9a9e06d5cc8a6a48
BrahiamAcosta <brahiam.acosta@udea.edu.co>
2024-09-07 15:40:39 -0500

    Initial Commit

---
+++ src/test/java/com/example/gestiondevuelos/GestionDeVuelosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestiondevuelos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeVuelosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
